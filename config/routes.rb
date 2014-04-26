7b63f32f781f1382204956be3ed07c8d50ecd310
Ali <write2ali@gmail.com>
2014-04-26 18:27:11 -0400

    added nav bar and links to ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Fithack::Application.routes.draw do
 
   root :to => "home#index"
-  get '/thoughts/:id', to: 'thoughts#show'
-  get '/challenges/:id', to: 'challenges#show'
-  get '/fitness/:id', to: 'fitness#show'
-  get '/rewards/:id', to: 'rewards#show'
-  get '/thoughts/:id', to: 'thoughts#show'
+  get '/thoughts/show', to: 'thoughts#show'
+  get '/my_day/show', to: 'challenges#show'
+  get '/health/show', to: 'fitness#show'
+  get '/rewards/show', to: 'rewards#show'
+  get '/thoughts/show', to: 'thoughts#show'
   
 end
