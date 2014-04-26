852b5d21c23712afd7638f23f37c6f1957d2b004
josthedudeoflife <josh.chou@ses-technologies.com>
2014-04-26 10:00:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fithack::Application.config.secret_key_base = '1042538d0842e5772faf18c051f741e7d37c0a76a1ddaabc45041ff8c9df64d2f21b0cfe711b2709488a6bbe26ec548d42d709d2caeb5954d486798e49e30fca'
