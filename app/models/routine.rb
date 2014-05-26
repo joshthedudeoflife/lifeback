77e86a5244dfb08c972e766ab23e9611c6befc27
josthedudeoflife <josh.chou@ses-technologies.com>
2014-05-26 14:44:33 -0400

    changed format of import csv

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -5,9 +5,15 @@ class Routine < ActiveRecord::Base
 	def self.import(file)
 		CSV.foreach(file.path, headers: true) do |row|
 			#creates new record
-			Routine.create! row.to_hash
+			#Routine.create! row.to_hash
+			routine_hash = row.to_hash # exclude the price field
+      routine = Routine.where(id: routine_hash["id"])
 
+      if routine.count == 1
+        routine.first.update_attributes(routine_hash)
+      else
+        routine.create!(routine_hash)
+			end
 		end
 	end
-
 end
