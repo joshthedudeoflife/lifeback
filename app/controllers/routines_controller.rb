2246fb71c5e392a802d5c462dd9f6f1348a6df71
josthedudeoflife <josh.chou@ses-technologies.com>
2015-02-08 15:07:32 -0500

    adding pie charts to routines

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -43,6 +43,108 @@ class RoutinesController < ApplicationController
       f.options[:chart][:defaultSeriesType] = "column"
       f.plot_options({:column=>{:stacking=>"percent"}})
     end
+    @chart = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Sleep',
+               :data=> [
+                  ['Firefox',   45.0],
+                  ['IE',       26.8],
+                  {
+                     :name=> 'Chrome',    
+                     :y=> 12.8,
+                     :sliced=> true,
+                     :selected=> true
+                  },
+                  ['Safari',    8.5],
+                  ['Opera',     6.2],
+                  ['Others',   0.7]
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = "Sleep Chart"
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+    end
+    @chartTwo = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Steps',
+               :data=> [
+                  ['Firefox',   45.0],
+                  ['IE',       26.8],
+                  {
+                     :name=> 'Chrome',    
+                     :y=> 12.8,
+                     :sliced=> true,
+                     :selected=> true
+                  },
+                  ['Safari',    8.5],
+                  ['Opera',     6.2],
+                  ['Others',   0.7]
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = "Steps Chart"
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+    end
+    @chartThree = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Distance',
+               :data=> [
+                  ['Firefox',   45.0],
+                  ['IE',       26.8],
+                  {
+                     :name=> 'Chrome',    
+                     :y=> 12.8,
+                     :sliced=> true,
+                     :selected=> true
+                  },
+                  ['Safari',    8.5],
+                  ['Opera',     6.2],
+                  ['Others',   0.7]
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = "Distance Chart"
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+    end
 	end
 	
 	def show
